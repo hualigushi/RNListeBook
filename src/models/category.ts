99db37a507a0aedcf64cb5819bb9ee33bb7632eb
money <hualigushi@163.com>
2021-01-17 21:29:39 +0800

    feat: 类别模块的model

---
+++ src/models/category.ts
@@ -0,0 +1,95 @@
+import storage, {load} from '@/config/storage';
+import axios from 'axios';
+import {Effect, Model, SubscriptionsMapObject} from 'dva-core-ts';
+import {Reducer} from 'redux';
+
+const CATEGORY_URL = '11';
+
+interface ICategory {
+  id: string;
+  name: string;
+  classify?: string;
+}
+interface CategoryModelState {
+  myCategorys: ICategory[];
+  categorys: ICategory[];
+}
+
+interface CategoryModel extends Model {
+  namespace: 'category';
+  state: CategoryModelState;
+  effects: {
+    loadData: Effect;
+  };
+  reducers: {
+    setState: Reducer<CategoryModelState>;
+  };
+  subscriptions: SubscriptionsMapObject;
+}
+
+const initialState: CategoryModelState = {
+  myCategorys: [
+    {
+      id: 'home',
+      name: '推荐',
+    },
+    {
+      id: 'vip',
+      name: 'Vip',
+    },
+  ],
+  categroys: [],
+};
+
+const categoryModel: CategoryModel = {
+  namespace: 'category',
+  state: initialState,
+  effects: {
+    *loadData(_, {call, put}) {
+      // storage获取数据
+      const myCategorys = yield call(load, {key: 'myCategorys'});
+      const categorys = yield call(load, {key: 'categorys'});
+      //发起action,将数据保存到state
+      if (myCategorys) {
+        yield put({
+          type: 'setState',
+          payload: {
+            myCategorys,
+            categorys,
+          },
+        });
+      } else {
+        yield put({
+          type: 'setState',
+          payload: {
+            categorys,
+          },
+        });
+      }
+    },
+  },
+  reducers: {
+    setState(state, {payload}) {
+      return {
+        ...state,
+        ...payload,
+      };
+    },
+  },
+  subscriptions: {
+    setup({dispatch}) {
+      dispatch({type: 'loadData'});
+    },
+    asyncStorage() {
+      storage.sync.categorys = async () => {
+        const {data} = await axios.get(CATEGORY_URL);
+        return data;
+      };
+      storage.sync.myCategorys = async () => {
+        return null;
+      };
+    },
+  },
+};
+
+export default categoryModel;
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,8 +1,10 @@
 import {DvaLoadingState} from 'dva-loading-ts';
+import category from './category';
 import home from './home';
-const models = [home];
+const models = [home, category];
 export type RootState = {
   home: typeof home.state;
+  category: typeof category.state;
   loading: DvaLoadingState;
 };
 export default models;
