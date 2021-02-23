f5b1704e09d4e37eba88d8ae8f8f2ef57f5fb49f
money <hualigushi@163.com>
2021-02-23 21:59:59 +0800

    feat: 账号模块的model

--- src/models/index.ts
+++ src/models/index.ts
@@ -4,12 +4,15 @@ import category from './category';
 import found from './found';
 import home from './home';
 import player from './player';
-const models = [home, category, album, player, found];
+import user from './user';
+
+const models = [home, category, album, player, found, user];
 export type RootState = {
   home: typeof home.state;
   category: typeof category.state;
   album: typeof album.state;
   player: typeof player.state;
+  user: typeof user.state;
   loading: DvaLoadingState;
 } & {
   [key: string]: typeof home.state;
---
+++ src/models/user.ts
@@ -0,0 +1,101 @@
+import axios from 'axios';
+import {Model, Effect, SubscriptionsMapObject} from 'dva-core-ts';
+import {Reducer} from 'redux';
+import storage, {storageLoad} from '@/config/storage';
+
+const USER_URL = '/mock/11/bear/login';
+
+export interface IUser {
+  name: string;
+  avatar: string;
+}
+
+export interface UserModelState {
+  // loging: boolean;
+  user?: IUser;
+}
+
+export interface UserModel extends Model {
+  namespace: 'user';
+  state: UserModelState;
+  effects: {
+    login: Effect;
+    logout: Effect;
+    // loadStorage: Effect;
+  };
+  reducers: {
+    setState: Reducer<UserModelState>;
+  };
+  // subscriptions: SubscriptionsMapObject;
+}
+
+const initialState = {
+  // loging: false,
+  user: undefined,
+};
+
+/**
+ * 登录模块的model
+ */
+const PlayerModel: UserModel = {
+  namespace: 'user',
+  state: initialState,
+  reducers: {
+    setState(state, {payload}) {
+      const newState = {
+        ...state,
+        ...payload,
+      };
+
+      return newState;
+    },
+  },
+  effects: {
+    // *loadStorage(_, {call, put}) {
+    //   const user = yield call(storageLoad, {key: 'user'});
+    //   yield put({
+    //     type: 'setState',
+    //     payload: {
+    //       loging: true,
+    //       user: user,
+    //     },
+    //   });
+    // },
+    *login({payload}, {call, put}) {
+      const {data, status} = yield call(axios.post, USER_URL, payload);
+      if (status === 100) {
+        yield put({
+          type: 'setState',
+          payload: {
+            // loging: true,
+            user: data,
+          },
+        });
+      }
+      // storage.save({
+      //   key: 'user', // 注意:请不要在key中使用_下划线符号!
+      //   data: data,
+      // });
+    },
+    *logout(_, {put}) {
+      yield put({
+        type: 'setState',
+        payload: {
+          // loging: false,
+          user: undefined,
+        },
+      });
+      // storage.save({
+      //   key: 'user', // 注意:请不要在key中使用_下划线符号!
+      //   data: null,
+      // });
+    },
+  },
+  // subscriptions: {
+  //   setup({dispatch}) {
+  //     dispatch({type: 'loadStorage'});
+  //   },
+  // },
+};
+
+export default userModel;
